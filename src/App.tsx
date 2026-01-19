8e2b61f80447f8c3586b8ecfca4d0be885997fbf
runpenguin <run-penguin@naver.com>
2026-01-19 18:08:54 +0900

    라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <>
       <div>리액트 프로젝트!</div>
+
+      <div className="wrapper">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </>
   );
 }
