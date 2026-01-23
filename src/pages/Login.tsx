cdce815dbe967115565276d8907b48fab85e48ec
runpenguin <run-penguin@naver.com>
2026-01-23 19:35:03 +0900

    로그인 페이지 이미지 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,16 @@
 import LoginList from "../features/login/LoginList";
+import "./Login.css";
 
 const Login = () => {
   return (
-    <>
-      <LoginList />
-    </>
+    <div className="login-page">
+      <div className="login-img-wrap">
+        <img src="/src/assets/hp-login.png" />
+      </div>
+      <div>
+        <LoginList />
+      </div>
+    </div>
   );
 };
 
