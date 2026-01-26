e0743658a6fd231d4dac83be58d657f19a2fd4c4
runpenguin <run-penguin@naver.com>
2026-01-26 17:21:02 +0900

    로그인 페이지에 로고 추가 및 전반적인 UI 변경

--- src/features/login/LoginList.tsx
+++ src/features/login/LoginList.tsx
@@ -3,28 +3,9 @@ import "./LoginList.css";
 const LoginList = () => {
   return (
     <div className="login-list">
-      <div className="general-wrap">
-        <label className="title">Login</label>
-
-        <div className="login-id">
-          <label></label>
-          <input type="text" placeholder="ID" />
-        </div>
-        <div className="login-pw">
-          <label></label>
-          <input type="password" placeholder="Password" />
-        </div>
-
-        <button type="button">로그인</button>
-      </div>
-
-      <div className="divide">
-        <label>OR</label>
-      </div>
-
-      <button type="button">카카오톡</button>
-      <button type="button">네이버</button>
-      <button type="button">구글</button>
+      <button type="button">카카오톡으로 로그인</button>
+      <button type="button">네이버로 로그인</button>
+      <button type="button">구글로 로그인</button>
     </div>
   );
 };
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,18 @@
 import LoginList from "../features/login/LoginList";
 import "./Login.css";
+import logo from "../assets/logo-text.png";
 
 const Login = () => {
   return (
     <div className="login-page">
+      <div className="logo-wrap">
+        <img src={logo} alt="Logo" />
+      </div>
+
       <div className="login-img-wrap">
         <img src="/src/assets/hp-login.png" />
       </div>
+
       <div>
         <LoginList />
       </div>
