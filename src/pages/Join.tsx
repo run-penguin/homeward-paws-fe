fb86ff73c06c0975990f5a97a47a317418c1ee9a
runpenguin <run-penguin@naver.com>
2026-02-04 19:11:32 +0900

    1. 일반 회원 가입 양식 디자인 2. 기본 폰트 변경

--- src/pages/Join.tsx
+++ src/pages/Join.tsx
@@ -1,6 +1,7 @@
+import "./Join.css";
+
 const Join = () => {
   const domainList = [
-    "선택해 주세요.",
     "naver.com",
     "hanmail.net",
     "daum.net",
@@ -14,50 +15,77 @@ const Join = () => {
 
   return (
     <div className="join-page">
-      <div className="page-title">회원 가입</div>
+      <div className="page-title">일반 회원 가입</div>
 
       <form>
         <div>
           <label>이메일</label>
-          <input type="text" />
-          <span>@</span>
-          <select>
-            {domainList.map((domain, idx) => (
-              <option key={idx} disabled={idx === 0} selected={idx === 0}>
-                {domain}
-              </option>
-            ))}
-          </select>
+          <div>
+            <input type="text" />
+            <span>@</span>
+            <div className="select-wrapper">
+              <select>
+                {domainList.map((domain, idx) => (
+                  <option key={idx}>{domain}</option>
+                ))}
+              </select>
+            </div>
+          </div>
+
+          <span className="warn-info">필수 입력 항목입니다.</span>
         </div>
 
         <div>
           <label>아이디</label>
           <input type="text" name="id" />
+          <span className="warn-info">필수 입력 항목입니다.</span>
         </div>
 
         <div>
           <label>비밀번호</label>
-          <span>영문, 숫자를 포함한 8자 이상의 비밀번호를 입력해 주세요.</span>
+          <span className="desc">
+            영문, 숫자를 포함한 8자 이상의 비밀번호를 입력해 주세요.
+          </span>
           <input type="passward" name="password" />
+          <span className="warn-info">필수 입력 항목입니다.</span>
         </div>
 
         <div>
           <label>비밀번호 확인</label>
           <input type="password " />
+          <span className="warn-info">
+            확인을 위해 비밀번호를 다시 입력해 주세요.
+          </span>
         </div>
 
         <div>
           <label>약관 동의</label>
 
-          <label>전체 동의</label>
-          <input type="checkbox" />
+          <div className="agree-wrapper">
+            <div>
+              <input type="checkbox" id="agreeAll" />
+              <label htmlFor="agreeAll">전체 동의</label>
+            </div>
 
-          <label>만 14세 이상입니다.(필수)</label>
-          <input type="checkbox" />
+            <div>
+              <input type="checkbox" id="agreeFourteen" />
+              <label htmlFor="agreeFourteen">
+                만 14세 이상입니다<span className="agree-required">(필수)</span>
+              </label>
+            </div>
 
-          <label>이용 약관(필수)</label>
-          <input type="checkbox" />
+            <div>
+              <input type="checkbox" id="agreeTerms" />
+              <label htmlFor="agreeTerms">
+                이용 약관<span className="agree-required">(필수)</span>
+              </label>
+            </div>
+          </div>
+
+          <span className="warn-info">약관에 동의해 주세요.</span>
         </div>
+
+        <button type="button">회원 가입하기</button>
       </form>
     </div>
   );
